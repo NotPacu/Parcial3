89b1ef9a64d341c18437e30b16098c1542aa14af
Jero <60076420+NotPacu@users.noreply.github.com>
2025-04-23 22:53:01 -0500

    cambios auth

--- Parcial3/App_Start/WebApiConfig.cs
+++ Parcial3/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Servicios_Jue.Handler;
 
 namespace Parcial3
 {
@@ -10,7 +11,7 @@ namespace Parcial3
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de Web API
-
+            config.MessageHandlers.Add(new TokenValidationHandler());
             // Rutas de Web API
             config.MapHttpAttributeRoutes();
 
